2b61aa072ce890f55a42f44b720b73387c1d8e08
Josival Salvador <josivaljunior087@gmail.com>
2025-08-23 06:45:15 -0400

    ajustando login

--- src/resources/categoria/categoria.router.ts
+++ src/resources/categoria/categoria.router.ts
@@ -3,14 +3,15 @@ import { Router } from 'express';
 import categoryController from './categoria.controller';
 import schema from './categoria.schema'
 import validate from '../../middlewares/validate';
+import isAuth from '../../middlewares/isAuth';
 
 const router = Router();
 
 // Category controller
 router.get('/', categoryController.index);
-router.post('/', validate(schema), categoryController.create);
+router.post('/', isAuth ,validate(schema), categoryController.create);
 router.get('/:id', categoryController.read);
-router.put('/:id', validate(schema), categoryController.update);
-router.delete('/:id', categoryController.remove);
+router.put('/:id', isAuth,validate(schema), categoryController.update);
+router.delete('/:id', isAuth,categoryController.remove);
 
 export default router;
